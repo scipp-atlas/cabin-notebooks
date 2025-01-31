a78a4014cd23837d7e6db9759410f1a1b6d5af70
Mike Hance <michael.hance@cern.ch>
2025-01-30 21:46:58 -0800

    adding ATLAS significance loss to init

--- LearningCutsUtils/__init__.py
+++ LearningCutsUtils/__init__.py
@@ -1,3 +1,3 @@
 from LearningCutsUtils.OneToOneLinear import OneToOneLinear
 from LearningCutsUtils.EfficiencyScanNetwork import EfficiencyScanNetwork
-from LearningCutsUtils.LossFunctions import lossvars, loss_fn, effic_loss_fn
+from LearningCutsUtils.LossFunctions import lossvars, loss_fn, effic_loss_fn, ATLAS_significance_loss
